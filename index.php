314a1b3f4c3ac789a52eb4bcf02a5da24038201c
Exotics <39382543+exotics1337@users.noreply.github.com>
2023-11-21 14:16:31 +0100

    Initial

---
+++ index.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>phQUIZ!</title>
+    <link rel="stylesheet" href="style.css" />
+</head>
+<body>
+    <div class="menu-selection">
+        <div class="menu-item play clickable"> <img /> PLAY </div>
+        <div class="menu-item config clickable"> <img /> CONFIG </div>
+        <div class="menu-item github clickable"> <img /> GITHUB </div>
+    </div>
+    <div class="footer">
+     <p>phQUIZ! <span>BETA-BUILD</span></p>
+        <p class="playing-as">playing as: <span class="username">ANON</span></p>
+    </div>
+    <div id="particles-js"></div>
+    <script src="https://cdn.jsdelivr.net/npm/particles.js@2.0.0/particles.min.js"></script>
+    <script src="app.js"></script>
+</body>
+</html>
